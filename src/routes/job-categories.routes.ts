18d65ba4c534e0ca26bca7c60a4469739d53458c
tanishq-chouhan-07 <tanishq.chouhan@gammaedge.io>
2026-02-03 12:29:12 +0530

    skills and job-categories routes and controller

---
+++ src/routes/job-categories.routes.ts
@@ -0,0 +1,8 @@
+import { Router } from "express";
+const jobCategoriesRouter = Router();
+import asyncHandler from "../middlewares/asyncHandler.js";
+import { getJobCategories } from "../controllers/job-categories.controller.js";
+
+jobCategoriesRouter.get("/", asyncHandler(getJobCategories))
+
+export default jobCategoriesRouter
