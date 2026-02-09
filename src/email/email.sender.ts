d4155ba6962ac442d6e836c4318cd493cb4b57c1
Shubham Kumar Gupta <shubham.kumar@gammaedge.io>
2026-02-09 16:46:17 +0530

    fix: replace nodemiler with emailjs to avoid smtp blockers

--- src/email/email.sender.ts
+++ src/email/email.sender.ts
@@ -1,21 +1,30 @@
 import { StatusCodes } from 'http-status-codes';
 import logger from '../config/logger.js';
-import transporter from '../config/nodemailer.config.js';
 import AppError from '../utils/AppError.js';
+import emailjs from '@emailjs/nodejs';
+import {
+  EMAILJS_PRIVATE_KEY,
+  EMAILJS_PUBLIC_KEY,
+  EMAILJS_SERVICE_ID,
+} from '../config/server-config.js';
 
-const sendEmail = async (receiverEmail: string, subject: string, emailTemplate: string) => {
+const sendOtp = async (receiverEmail: string, otp: string, templateId: string) => {
   try {
-    const info = await transporter.sendMail({
-      from: 'hirehub-noreply.hirehub.io',
-      to: receiverEmail,
-      subject: subject,
-      html: emailTemplate,
-    });
-    logger.info(`email sent, messageId: ${info.messageId}`);
+    const res = await emailjs.send(
+      EMAILJS_SERVICE_ID!,
+      templateId!,
+      { email: receiverEmail, otp },
+      {
+        privateKey: EMAILJS_PRIVATE_KEY!,
+        publicKey: EMAILJS_PUBLIC_KEY!
+      },
+    );
+    logger.info(`email sent, status: ${res.status}`);
+    return true;
   } catch (error) {
     logger.error(`failed to sent email, ${error}`);
     throw new AppError(`Failed to sent email`, StatusCodes.INTERNAL_SERVER_ERROR);
   }
 };
 
-export default sendEmail;
+export default sendOtp;
