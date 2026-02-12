fa0ddf26212e7d6e3f00b2d21a3e2117564ef6eb
Shubham Kumar Gupta <shubham.kumar@gammaedge.io>
2026-02-12 18:33:37 +0530

    feat: implement newsletter subscription flow

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -2,10 +2,12 @@ import express from 'express';
 import jobRoutes from './jobs.route.js';
 import empJobRoutes from './emp.jobs.routes.js';
 import userRoutes from './user.routes.js';
+import subscriptionRoutes from './subscription.route.js'
 const router = express.Router();
 
 router.use('/employer', empJobRoutes);
 router.use('/jobs', jobRoutes);
 router.use('/auth', userRoutes);
+router.use('/subscribe', subscriptionRoutes);
 
 export default router;
