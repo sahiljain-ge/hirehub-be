53abd086169c7fe06adc5654756e680d679f40db
Shubham Kumar Gupta <shubham.kumar@gammaedge.io>
2026-02-16 12:13:08 +0530

    fix: skills and categories route mapping

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -4,12 +4,16 @@ import empJobRoutes from './emp.jobs.routes.js';
 import userRoutes from './user.routes.js';
 import subscriptionRoutes from './subscription.route.js';
 import testimonialsRoutes from './testimonials.routes.js';
+import categoriesRoutes from './job-categories.routes.js';
+import skillsRoutes from './skills.route.js';
 
 const router = express.Router();
 
 router.use('/employer', empJobRoutes);
 router.use('/jobs', jobRoutes);
 router.use('/auth', userRoutes);
+router.use('/job-categories', categoriesRoutes);
+router.use('/skills', skillsRoutes);
 router.use('/subscribe', subscriptionRoutes);
 router.use('/testimonials', testimonialsRoutes);
 
