51781ef9c3ba8e0c6d66e58e2975c987b3f0fe49
AyushSharma72G <ayush.sharma@gammaedge.io>
2026-02-18 17:47:45 +0530

    implemented suggested changes in job Controller

--- src/repositories/jobSeeker.repository.ts
+++ src/repositories/jobSeeker.repository.ts
@@ -1,75 +1,81 @@
 import { UUID } from 'node:crypto';
 import db from '../config/prisma.js';
-
-
+import AppError from '../utils/AppError.js';
+import { StatusCodes } from 'http-status-codes';
 class JobSeekerRepository {
-    async getSkills(seekerId: UUID) {
-        try {
-            const skills = await db.jobSeekerSkill.findMany({
-                where: {
-                    seeker_id: seekerId   // get the skills which have seeker id what we got from the auth middleware 
-                },
-                select: {
-                    skill: {
-                        select: {
-                            id: true,
-                            name: true
-                        }
-                    }
-                }
-            });
+  async getSkills(userId: UUID) {
+    try {
+      const seeker = await db.jobSeeker.findUnique({
+        where: {
+          user_id: userId,
+        },
+      });
 
-            return skills;
-        } catch (error) {
-            throw error;
-        }
+      if (!seeker) {
+        throw new AppError('Job seeker Id not found', StatusCodes.UNAUTHORIZED);
+      }
+      const skills = await db.jobSeekerSkill.findMany({
+        where: {
+          seeker_id: seeker.id, // get the skills which have seeker id what we got from the auth middleware
+        },
+        select: {
+          skill: {
+            select: {
+              id: true,
+              name: true,
+            },
+          },
+        },
+      });
+
+      return skills;
+    } catch (error) {
+      throw error;
     }
+  }
 
-    async addSkills(seekerId: UUID, skillIds: number[]) {
-        try {
-            return await db.jobSeekerSkill.createMany({
-                data: skillIds.map((skillId) => ({
-                    seeker_id: seekerId,  // creating the data object we have to insert
-                    skill_id: skillId,
-                })),
-                skipDuplicates: true,
-            });
-        } catch (error) {
-            throw error;
-        }
+  async addSkills(seekerId: UUID, skillIds: number[]) {
+    try {
+      return await db.jobSeekerSkill.createMany({
+        data: skillIds.map((skillId) => ({
+          seeker_id: seekerId, // creating the data object we have to insert
+          skill_id: skillId,
+        })),
+        skipDuplicates: true,
+      });
+    } catch (error) {
+      throw error;
     }
+  }
 
-    async deleteSkills(seekerId: UUID, skillIds: number[]) {
-        try {
-            return await db.jobSeekerSkill.deleteMany({
-                where: {
-                    seeker_id: seekerId,
-                    skill_id: {
-                        in: skillIds,   // only delete the skills sent from the frontend 
-                    },
-                },
-            });
-        } catch (error) {
-            throw error;
-        }
+  async deleteSkills(seekerId: UUID, skillIds: number[]) {
+    try {
+      return await db.jobSeekerSkill.deleteMany({
+        where: {
+          seeker_id: seekerId,
+          skill_id: {
+            in: skillIds, // only delete the skills sent from the frontend
+          },
+        },
+      });
+    } catch (error) {
+      throw error;
     }
+  }
 
-
-    async deleteSingleSkill(seekerId: UUID, skillId: number) {
-        try {
-            return await db.jobSeekerSkill.deleteMany({
-                where: {
-                    seeker_id: seekerId,
-                    skill_id: skillId,
-                },
-            });
-        } catch (error) {
-            throw error;
-        }
+  async deleteSingleSkill(seekerId: UUID, skillId: number) {
+    try {
+      return await db.jobSeekerSkill.deleteMany({
+        where: {
+          seeker_id: seekerId,
+          skill_id: skillId,
+        },
+      });
+    } catch (error) {
+      throw error;
     }
-
-
-
+  }
 }
 
 export default JobSeekerRepository;
+
--- src/services/jobSeeker.service.ts
+++ src/services/jobSeeker.service.ts
@@ -1,27 +1,25 @@
-import JobSeekerRepository from '../repositories/jobSeeker.repository.js';;
+import JobSeekerRepository from '../repositories/jobSeeker.repository.js';
 import { UUID } from 'node:crypto';
 
 class JobSeekerService {
-    constructor(private readonly jobSeekerRepository: JobSeekerRepository) { }
+  constructor(private readonly jobSeekerRepository: JobSeekerRepository) {}
 
-    async getSkills(seekerId: UUID) {
-        const skills = await this.jobSeekerRepository.getSkills(seekerId);
+  async getSkills(seekerId: UUID) {
+    const skills = await this.jobSeekerRepository.getSkills(seekerId);
+    return skills.map((s) => s.skill);
+  }
 
-        return skills.map(s => s.skill);
-    }
+  async addSkills(userId: UUID, skillIds: number[]) {
+    return await this.jobSeekerRepository.addSkills(userId, skillIds);
+  }
+  async deleteSkills(seekerId: UUID, skillIds: number[]) {
+    return await this.jobSeekerRepository.deleteSkills(seekerId, skillIds);
+  }
 
-
-    async addSkills(seekerId: UUID, skillIds: number[]) {
-        return await this.jobSeekerRepository.addSkills(seekerId, skillIds);
-    }
-    async deleteSkills(seekerId: UUID, skillIds: number[]) {
-        return await this.jobSeekerRepository.deleteSkills(seekerId, skillIds);
-    }
-
-    async deleteSingleSkill(seekerId: UUID, skillId: number) {
-        return this.jobSeekerRepository.deleteSingleSkill(seekerId, skillId);
-    }
-
+  async deleteSingleSkill(seekerId: UUID, skillId: number) {
+    return this.jobSeekerRepository.deleteSingleSkill(seekerId, skillId);
+  }
 }
 
 export default JobSeekerService;
+
