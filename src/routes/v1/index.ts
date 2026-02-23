ffcec5d0177d8fe2eced6e99a20070a61affaa72
devrajkurmi-web <devraj.kurmi@gammaedge.io>
2026-02-23 10:42:06 +0530

    company profile routes mapping

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -10,6 +10,7 @@ import jobSeekerRoutes from './job-seeker-profile.routes.js';
 import applicationRoutes from './application.routes.js';
 import addressRoutes from './address.routes.js';
 import JobSeekerRoutes from "./jobSeekerSkills.routes.js"
+import companyRoutes from './company.routes.js'
 
 const router = express.Router();
 router.use('/employer', empJobRoutes);
@@ -23,8 +24,7 @@ router.use('/applications', applicationRoutes);
 router.use('/job-seeker/profile', jobSeekerRoutes);
 router.use('/applications', applicationRoutes);
 router.use('/addresses', addressRoutes);
-
-
+router.use('/company', companyRoutes)
 router.use("/job-seeker/skills", JobSeekerRoutes)
 
 export default router;
