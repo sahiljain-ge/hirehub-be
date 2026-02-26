0e579b8a077ee6cbbe780f8085ffdec3ee23c0b2
sahil jain <sahil.jain@gammaedge.io>
2026-02-26 17:13:56 +0530

    verify is user still exists or not

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -4,14 +4,16 @@ import { Role } from '../generated/enums.js';
 import { verifyAccessToken } from '../utils/jwt.js';
 import { StatusCodes } from 'http-status-codes';
 import { sendFail } from '../utils/responseFormatter.js';
+import db from '../config/prisma.js';
 
 interface JwtPayload {
   id: string;
   email: string;
   role: string;
+  company_id?: string;
 }
 
-const authMiddleware = (req: Request, _res: Response, next: NextFunction) => {
+const authMiddleware = async (req: Request, _res: Response, next: NextFunction) => {
   const authHeader = req.headers.authorization;
 
   if (!authHeader || !authHeader.startsWith('Bearer ')) {
@@ -23,11 +25,36 @@ const authMiddleware = (req: Request, _res: Response, next: NextFunction) => {
   try {
     const decoded = verifyAccessToken(token) as JwtPayload;
 
-    req.user = {
+    const user = await db.user.findFirst({
+      where: {
+        id: decoded.id,
+      },
+    });
+
+    if (!user) throw new AppError('User not found', StatusCodes.UNAUTHORIZED);
+
+    if (user.role == Role.EMPLOYER) {
+      const company = await db.company.findUnique({
+        where: {
+          employer_id: user.id
+        }
+      })
+      if (!company) throw new AppError('Incomplete company Profile', StatusCodes.BAD_REQUEST
+
+      )
+      req.user = {
+        id: decoded.id,
+        email: decoded.email,
+        role: decoded.role as Role,
+        company_id: company?.id
+      };
+    } else {
+      req.user = {
       id: decoded.id,
       email: decoded.email,
       role: decoded.role as Role,
-    };
+      };
+    }
 
     next();
   } catch {
