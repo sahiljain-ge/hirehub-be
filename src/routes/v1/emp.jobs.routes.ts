d3c9ebc330bc56c7cbd25df0499aedfb6e35cbe3
sahil jain <sahil.jain@gammaedge.io>
2026-03-04 15:38:26 +0530

    hotFix: is company profile completed validation

--- src/routes/v1/emp.jobs.routes.ts
+++ src/routes/v1/emp.jobs.routes.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import { jobController } from './jobs.route.js';
 import asyncHandler from '../../middlewares/asyncHandler.js';
-import authMiddleware from '../../middlewares/auth.middleware.js';
+import authMiddleware, { requireCompleteCompanyProfile } from '../../middlewares/auth.middleware.js';
 import { requireEmployer } from '../../middlewares/auth.middleware.js';
 import { validateJobData } from '../../validators/jobs.validator.js';
 import { createJobSchema } from '../../schemas/jobs.schema.js';
@@ -10,7 +10,7 @@ const router = express.Router();
 
 router.use(authMiddleware);
 router.use(requireEmployer);
-
+router.use(requireCompleteCompanyProfile)
 router.post('/jobs', validateJobData(createJobSchema), asyncHandler(jobController.createJob));
 router.get('/jobs', asyncHandler(jobController.getAllJobsOfEmp));
 router.patch('/jobs/:id/close', asyncHandler(jobController.updateJobStatus));
